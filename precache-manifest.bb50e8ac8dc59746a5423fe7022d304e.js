e1ef6303a44bfa027faaddd1bd5b1f6432b03f0f
Daniel Lopez <daniel94lad@gmail.com>
2020-04-27 19:52:14 -0500

    Updates

---
+++ precache-manifest.bb50e8ac8dc59746a5423fe7022d304e.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c7199ecf6a61aa7d808ee499456eb0",
+    "url": "/danielWebDev/index.html"
+  },
+  {
+    "revision": "e98fcdad8d06cd695058",
+    "url": "/danielWebDev/static/css/2.3db4eb7d.chunk.css"
+  },
+  {
+    "revision": "f3f6a2861f2e2d9877da",
+    "url": "/danielWebDev/static/css/main.297ebd0f.chunk.css"
+  },
+  {
+    "revision": "e98fcdad8d06cd695058",
+    "url": "/danielWebDev/static/js/2.2b177c5c.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/danielWebDev/static/js/2.2b177c5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f6a2861f2e2d9877da",
+    "url": "/danielWebDev/static/js/main.ed0cbd40.chunk.js"
+  },
+  {
+    "revision": "b310d39e67066328dfee",
+    "url": "/danielWebDev/static/js/runtime-main.c2bbb80b.js"
+  },
+  {
+    "revision": "0d44edf2a0587f889b361410915aceab",
+    "url": "/danielWebDev/static/media/About-me.0d44edf2.svg"
+  },
+  {
+    "revision": "845262e496cf9e5753ab227e1f1bbd24",
+    "url": "/danielWebDev/static/media/Arduino-Logo.845262e4.svg"
+  },
+  {
+    "revision": "e9e848930ff0259309eddc5e18a768d0",
+    "url": "/danielWebDev/static/media/ArtPortafolio.e9e84893.png"
+  },
+  {
+    "revision": "6b61b55c2ba2b66c7ac8b1046218466f",
+    "url": "/danielWebDev/static/media/Bonanza.6b61b55c.jpg"
+  },
+  {
+    "revision": "b15af9abb8494ff9f551d7da424d4349",
+    "url": "/danielWebDev/static/media/Bunnys-02.b15af9ab.JPG"
+  },
+  {
+    "revision": "5bf0e4e7db6274a5e36670034a5fbdec",
+    "url": "/danielWebDev/static/media/C-Logo.5bf0e4e7.svg"
+  },
+  {
+    "revision": "45236da06863b25a99e2dfd7a0ef1450",
+    "url": "/danielWebDev/static/media/Cell-Icon.45236da0.svg"
+  },
+  {
+    "revision": "fcd39df7de11386518d2cd7c8db8ac90",
+    "url": "/danielWebDev/static/media/CodePart.fcd39df7.jpg"
+  },
+  {
+    "revision": "9bb369f60d4e5e26449290eb83f4bdfa",
+    "url": "/danielWebDev/static/media/CodeWords-Back.9bb369f6.jpg"
+  },
+  {
+    "revision": "657a5dab799f7305813769a5000d1956",
+    "url": "/danielWebDev/static/media/ColorPart.657a5dab.jpg"
+  },
+  {
+    "revision": "84ab417272ec942243888978891f3813",
+    "url": "/danielWebDev/static/media/Daniel Lopez T. - CV (Eng) 2020_Vlow.84ab4172.pdf"
+  },
+  {
+    "revision": "879ae84b9e99c6551db515e081cfbe80",
+    "url": "/danielWebDev/static/media/Game-Icon.879ae84b.svg"
+  },
+  {
+    "revision": "82905cdb3a301098c2dabe3557a7cb4c",
+    "url": "/danielWebDev/static/media/Linkedin.82905cdb.svg"
+  },
+  {
+    "revision": "33962392e613f6a8cd46b88bd4d4cdaf",
+    "url": "/danielWebDev/static/media/Map-Icon.33962392.svg"
+  },
+  {
+    "revision": "93d156bd2c1bd3e2f5aa3ac61b53f1bc",
+    "url": "/danielWebDev/static/media/Maya-Logo.93d156bd.svg"
+  },
+  {
+    "revision": "12e39c64db5d116bdc93fb76e7ff696a",
+    "url": "/danielWebDev/static/media/Mylogo-D1.12e39c64.svg"
+  },
+  {
+    "revision": "2431da82349e2225169bf421f55d177c",
+    "url": "/danielWebDev/static/media/Orquesta-01.2431da82.jpg"
+  },
+  {
+    "revision": "f25a9d6893d9cc88fd461e1838e6fcf1",
+    "url": "/danielWebDev/static/media/Painter-Logo.f25a9d68.svg"
+  },
+  {
+    "revision": "f01d258e59a47be5544073f67a977678",
+    "url": "/danielWebDev/static/media/PyPartyImg01.f01d258e.JPG"
+  },
+  {
+    "revision": "26a40aee1136fb5f605f30fef125efc8",
+    "url": "/danielWebDev/static/media/UX-Icon.26a40aee.svg"
+  },
+  {
+    "revision": "d304562545240221db746a1b33c665a8",
+    "url": "/danielWebDev/static/media/Unity-Logo.d3045625.svg"
+  },
+  {
+    "revision": "87d45fda349472383384acf8cdc29e6e",
+    "url": "/danielWebDev/static/media/Unreal-05.87d45fda.JPG"
+  },
+  {
+    "revision": "84785254fdc1aa729f8a8597e83598e0",
+    "url": "/danielWebDev/static/media/WebDev-Icon.84785254.svg"
+  },
+  {
+    "revision": "dec21930a1a4f59520b5c55bff21dc6f",
+    "url": "/danielWebDev/static/media/github.dec21930.svg"
+  }
+]);
